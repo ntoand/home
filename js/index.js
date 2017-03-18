2afccf626a2d24fed7272401abef43c49bdfcb1b
Toan Nguyen <ntoand@gmail.com>
2017-03-18 17:30:59 +1100

    basic index page layout

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
-(function ($) {
+jQuery('#section-skills').one('inview', function (event, visible) {
+  console.log(event);
+  console.log('Here');
+  if (visible == true) {
+    console.log("inview");
+
+  }
+});
+
+jQuery(document).ready(function(){
   "use strict";
 
   // particles
@@ -6,6 +15,35 @@
     console.log('callback - particles.js config loaded');
   });
 
-  //
+  // skillbar
+  /*
+  jQuery('.skillbar').each(function(){
+    jQuery(this).find('.skillbar-bar').animate({
+      width:jQuery(this).attr('data-percent')
+    },5000);
+  });
+  */
 
-})(jQuery);
+  $(window).scroll( function(){
+
+    /* Check the location of each desired element */
+    jQuery('.skillbar').each( function(i){
+
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it in */
+      if( bottom_of_window > bottom_of_object ){
+
+        jQuery(this).find('.skillbar-bar').animate({
+          width:jQuery(this).attr('data-percent')
+        },2000);
+
+      }
+
+    });
+
+  });
+
+
+});
