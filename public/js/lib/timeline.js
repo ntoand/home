5699d5375a5a54360bdb246448457a8599e9d498
Toan Nguyen <ntoand@gmail.com>
2017-03-22 14:32:54 +1100

    fix timeline animation

--- public/js/lib/timeline.js
+++ public/js/lib/timeline.js
@@ -20,10 +20,8 @@ jQuery(document).ready(function($){
 	}
 
 	function showBlocks(blocks, offset) {
-		/*
 		blocks.each(function(){
 			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		});
-		*/
 	}
 });
